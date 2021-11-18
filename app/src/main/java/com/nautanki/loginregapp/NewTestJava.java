c616ba68691d8caa83933db7b92395239b920871
khalid <khalidsyfullah3@gmail.com>
2021-11-18 16:53:21 +0600

    Commit From Android Studio

--- app/src/main/java/com/nautanki/loginregapp/NewTestJava.java
+++ app/src/main/java/com/nautanki/loginregapp/NewTestJava.java
@@ -1,4 +1,24 @@
 package com.nautanki.loginregapp;
 
 public class NewTestJava {
+    String val1, val2;
+    int v;
+
+    public NewTestJava(String val1, String val2, int v) {
+        this.val1 = val1;
+        this.val2 = val2;
+        this.v = v;
+    }
+
+    public String getVal1() {
+        return val1;
+    }
+
+    public String getVal2() {
+        return val2;
+    }
+
+    public int getV() {
+        return v;
+    }
 }
